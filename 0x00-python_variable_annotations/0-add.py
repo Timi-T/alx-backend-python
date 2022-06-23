ff5ee8fb531afab16d524b017c73cfb112310199
Timi-T <timiopebode@gmail.com>
2022-06-23 08:34:14 +0100

    pycodestyle for 1-add.py and task 1 file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module to define a typed annotated function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Type annotated function to add two float numbers"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module to define a type annotated function which tes in string
+argumnts and returns a string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function to concatenate two strings"""
+    return str1 + str2

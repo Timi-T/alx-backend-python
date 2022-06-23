a358fb9a59f20739999c55df583426b220c21a33
Timi-T <timiopebode@gmail.com>
2022-06-23 08:47:18 +0100

    define variables with type annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module to define a type annotated function which declares type
+annotated variables
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"

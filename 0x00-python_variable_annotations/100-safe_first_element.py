149667216df621ee1a1e2efe90056c7f62057ff3
Timi-T <timiopebode@gmail.com>
2022-06-23 14:01:46 +0100

    pycodestyle

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,11 +3,10 @@
 Module to define a type annotated function which takes in any data type
 and returns either none or anydatatype
 """
-from types import NoneType
 from typing import Any, Union, Sequence
 
 
-def safe_first_element(lst: Sequence[Any]) -> Any | None:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Return the input typecasted to a list"""
     if lst:
         return lst[0]

9c423fc4b60cbd1be3450de6993bd73b174ec32a
Timi-T <timiopebode@gmail.com>
2022-06-23 13:53:24 +0100

    typevar for return value

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,9 +6,9 @@ and returns either none or anydatatype
 from typing import Any, Mapping, TypeVar, Union
 
 
-T = TypeVar("T", str, bytes)
-def safely_get_value(dct: Mapping, key: Any, default: None):
-    """Function to return a value from a dictionary using its key"""
+T = TypeVar("T")
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:
+    """Function to return a value from a dictionary using its"""
     if key in dct:
         return dct[key]
     else:

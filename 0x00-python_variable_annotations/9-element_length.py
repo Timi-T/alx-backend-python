4120c7a19743806c72f67cba52adb3df8de760ee
Timi-T <timiopebode@gmail.com>
2022-06-23 09:38:33 +0100

    annotate an existing function

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module to annotate a function's parameters and return values
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]

a7c1eec4a197f1c53873c7d68a645ad690e6b85f
Timi-T <timiopebode@gmail.com>
2022-06-23 08:43:01 +0100

    comverted a float to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module to define a type annotated function which takes in a float
+argumnt and returns a string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """Function to convert a float to a string"""
+    return str(n)

37a18bec5bd074dde2a8fbc06f47ef4a14052956
Timi-T <timiopebode@gmail.com>
2022-06-27 16:40:04 +0100

    await a task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 """
-Calculate runtime of an async function
+Module to get a list from an asynchronous function
 """
 
-import time
-wait_n = __import__('1-concurrent_coroutines').wait_n
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> float:
-    """Function to measure runtime of an async function"""
-    my_list = []
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Function to call wait_random n times"""
+    ret_list = []
     for i in range(n):
-        start = time.time()
-        await wait_n(n, max_delay)
-        end = time.time()
-        my_list.append((end - start) / n)
-    return my_list
+        task = task_wait_random(max_delay)
+        ret_list.append(await task)
+        j = i
+        while j != 0:
+            if ret_list[j] < ret_list[j - 1]:
+                temp = ret_list[j]
+                ret_list[j] = ret_list[j - 1]
+                ret_list[j - 1] = temp
+            j -= 1
+    return ret_list

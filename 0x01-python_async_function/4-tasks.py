207c1ae73a43f78fe5c199b8fef575117a6c8a0a
Timi-T <timiopebode@gmail.com>
2022-06-27 16:33:50 +0100

    await a task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,11 +3,16 @@
 Calculate runtime of an async function
 """
 
-import asyncio
+import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def task_wait_n(n: int, max_delay: int) -> float:
     """Function to measure runtime of an async function"""
-    task = asyncio.create_task(wait_n(n, max_delay))
-    return await task
+    my_list = []
+    for i in range(n):
+        start = time.time()
+        await wait_n(n, max_delay)
+        end = time.time()
+        my_list.append((end - start) / n)
+    return my_list

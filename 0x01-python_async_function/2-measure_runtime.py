37c52f565d84448dc675eb4a9d98e8d5aa137d38
Timi-T <timiopebode@gmail.com>
2022-06-27 20:50:47 +0100

    non async function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -3,15 +3,14 @@
 Calculate runtime of an async function
 """
 
-import time
 import asyncio
-from tracemalloc import start
+import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Function to measure runtime of an async function"""
     start = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end = time.time()
     return (end - start) / n

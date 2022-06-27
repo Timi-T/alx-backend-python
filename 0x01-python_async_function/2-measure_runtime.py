7307fce5fc4e5f2ec9d9e9be167a67d330514d90
Timi-T <timiopebode@gmail.com>
2022-06-27 15:54:44 +0100

    count run time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Calculate runtime of an async function
+"""
+
+import time
+import asyncio
+from tracemalloc import start
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Function to measure runtime of an async function"""
+    start = time.time()
+    await wait_n(n, max_delay)
+    end = time.time()
+    return (end - start) / n

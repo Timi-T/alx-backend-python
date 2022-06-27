1554eaa394786198695f34ae5ac4eadd933b72fa
Timi-T <timiopebode@gmail.com>
2022-06-27 13:09:50 +0100

    removed sort method

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,18 @@ Module to get a list from an asynchronous function
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> list:
     """Function to call wait_random n times"""
-    ret_list = [await wait_random(max_delay) for i in range(n)]
-    return ret_list.sort()
+    ret_list = []
+    for i in range(n):
+        val = await wait_random(max_delay)
+        ret_list.append(val)
+        j = i
+        while j != 0:
+            if ret_list[j] < ret_list[j - 1]:
+                temp = ret_list[j]
+                ret_list[j] = ret_list[j - 1]
+                ret_list[j - 1] = temp
+            j -= 1
+    return ret_list

5953456ca00731418c116de1ca0fbd44c59f9675
Timi-T <timiopebode@gmail.com>
2022-06-27 12:25:22 +0100

    type annotate function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ Basic syntac for async await functions
 from random import uniform
 
 
-async def wait_random(max_delay = 10.0):
+async def wait_random(max_delay: float = 10.0) -> float:
     """Function to wait for a random delay"""
 
     return uniform(0, max_delay)

a6e6f3f9c38b65897bb8ca2d7cccf73020716453
Timi-T <timiopebode@gmail.com>
2022-06-27 14:23:27 +0100

    pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,8 +3,8 @@
 Module to get a list from an asynchronous function
 """
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:

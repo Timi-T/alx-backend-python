1208dd31f384112cbe211865cdb39fc90def0525
Timi-T <timiopebode@gmail.com>
2022-07-16 11:43:10 +0100

    slight change

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,13 +34,12 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_method.return_value = ret
             self.assertEqual(test_client._public_repos_url, ret["repos_url"])
 
-    @mock.patch('client.get_json', return_value=[{"name": "PL1"},
-                {"name": "PL2", "license": "ok"}, {"name": "PL3"}])
+    @patch('client.get_json', return_value=[{"name": "PL1"},
+           {"name": "PL2", "license": "ok"}, {"name": "PL3"}])
     def test_public_repos(self, payloads):
         """Test public_repos method"""
         with mock.patch('client.GithubOrgClient._public_repos_url',
                         new_callable=PropertyMock) as mock_method:
-            mock_method.return_value = "Payloads"
             test_client = client.GithubOrgClient("ope")
             self.assertEqual(test_client.public_repos(),
                              [repo['name'] for repo in payloads.return_value])
